25ace8f1814f22bffdef149820863b482c4ebd15
Mateus Alves Conceição <mateus.alvesc02@gmail.com>
2022-11-13 12:19:23 -0300

    Upload files

---
+++ src/br/com/alura/rh/model/Terceirizado.java
@@ -0,0 +1,22 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+
+public class Terceirizado {
+
+    private DadosPessoais dadosPessoais;
+    private String empresa;
+
+    public Terceirizado(DadosPessoais dadosPessoais, String empresa) {
+        this.dadosPessoais = dadosPessoais;
+        this.empresa = empresa;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+}
